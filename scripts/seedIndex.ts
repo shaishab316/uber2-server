b92569c195f7e1c49465891f7e887d5eb339c8c9
shaishab316 <shaishabchandrashil@gmail.com>
2025-10-20 14:08:16 +0600

    refactor: Refactor seedIndex script for direct index creation
    Refactors the `seedIndex.ts` script to execute index creation logic
    directly in an IIFE and removes unnecessary index creations for
    available drivers and trips. It now focuses solely on the users
    collection and uses `console.log` instead of the `logger` utility.

--- scripts/seedIndex.ts
+++ scripts/seedIndex.ts
@@ -1,60 +1,27 @@
+/* eslint-disable no-console */
 import chalk from 'chalk';
-import { logger } from '../src/utils/logger';
 import { MongoClient } from 'mongodb';
 import config from '../src/config';
 
-export async function setupIndexes() {
+(async () => {
   const client = new MongoClient(config.url.database);
 
-  logger.info(chalk.green('🚀 Database connecting...'));
+  console.log(chalk.green('🚀 Database connecting...'));
   await client.connect();
-  logger.info(chalk.green('🚀 Database connected successfully'));
+  console.log(chalk.green('🚀 Database connected successfully'));
 
   const db = client.db(config.server.db_name);
 
-  logger.info(chalk.green('🔑 DB Indexes setup started...'));
+  console.log(chalk.green('🔑 DB Indexes setup started...'));
   try {
-    {
-      await db
-        ?.collection('available_drivers')
-        .createIndex(
-          { 'location.geo': '2dsphere' },
-          { background: true, name: 'location__2dsphere' },
-        );
-    }
-
-    {
-      await db
-        ?.collection('trips')
-        .createIndex(
-          { 'pickup_address.geo': '2dsphere' },
-          { background: true, name: 'pickup_address__2dsphere' },
-        );
-
-      await db
-        ?.collection('trips')
-        .createIndex(
-          { 'dropoff_address.geo': '2dsphere' },
-          { background: true, name: 'dropoff_address__2dsphere' },
-        );
-
-      await db
-        ?.collection('trips')
-        .createIndex(
-          { 'stops.geo': '2dsphere' },
-          { background: true, name: 'stops__2dsphere' },
-        );
-    }
-
-    {
-      await db
-        ?.collection('users')
-        .createIndex(
-          { 'location.geo': '2dsphere' },
-          { background: true, name: 'location__2dsphere' },
-        );
-    }
+    await db
+      .collection('users')
+      .createIndex(
+        { 'location.geo': '2dsphere' },
+        { background: true, name: 'location__2dsphere' },
+      );
   } finally {
-    logger.info(chalk.green('✅ DB Indexes setup successfully'));
+    console.log(chalk.green('✅ DB Indexes setup successfully'));
+    await client.close();
   }
-}
+})();
