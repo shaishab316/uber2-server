01c08c565965a295b052628fb28e28ee27ecfe92
shaishab316 <shaishabchandrashil@gmail.com>
2025-11-08 16:58:37 +0600

    Stop tracking migrations folder

--- src/app/modules/migrations/20251022114020_/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."parcels" ADD COLUMN     "location_address" TEXT,
-ADD COLUMN     "location_lat" DOUBLE PRECISION,
-ADD COLUMN     "location_lng" DOUBLE PRECISION,
-ADD COLUMN     "location_type" TEXT DEFAULT 'Point';
--- src/app/modules/migrations/20251023103301_review/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `last_edited` to the `reviews` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "reviews" ADD COLUMN     "last_edited" TIMESTAMP(3) NOT NULL;
