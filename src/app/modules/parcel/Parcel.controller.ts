b6444df1db1dbdfab8aaac9051823c808651db9c
shaishab316 <shaishabchandrashil@gmail.com>
2026-02-04 19:56:13 +0600

    refactor: update parcel acceptance notification to use new event structure

--- src/app/modules/parcel/Parcel.controller.ts
+++ src/app/modules/parcel/Parcel.controller.ts
@@ -200,19 +200,10 @@ export const ParcelControllers = {
 
       if (parcel.user_id) {
         //? Notify user that their parcel has been accepted
-        SocketServices.emitToUser(parcel.user_id, 'parcel:accepted', {
-          driver: await prisma.user.findUnique({
-            where: {
-              id: driver.id,
-            },
-            omit: userOmit.DRIVER,
-          }),
-          parcel,
-
-          /**
-           * Todo: fix driver emit data type
-           */
-        });
+        SocketServices.emitToUser(parcel.user_id, 'user-trip', {
+          kind: RIDE_KIND.PARCEL,
+          data: parcel,
+        } satisfies TRideResponseV2);
       }
 
       return {
